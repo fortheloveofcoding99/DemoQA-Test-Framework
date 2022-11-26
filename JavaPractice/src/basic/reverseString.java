43e4fa7bd5ebf2253783a5b0bf982694dbf80b52
Somnath Sarkar <somsarkar9698@gmail.com>
2022-11-26 08:19:18 +0530

    from intellij intPracticeDay1

--- JavaPractice/src/basic/reverseString.java
+++ JavaPractice/src/basic/reverseString.java
@@ -4,6 +4,20 @@ public class reverseString {
 
     String methodStringReverse(String x)
     {
+        char [] y = x.toCharArray();
+        String revString = "";
 
+        for (int i=y.length-1;i>=0;i--)
+        {
+            revString = revString+y[i];
+        }
+
+         return revString;
+    }
+
+    public static void main(String[] args) {
+
+        reverseString rs = new reverseString();
+        System.out.println(rs.methodStringReverse("Gluck"));
     }
 }
